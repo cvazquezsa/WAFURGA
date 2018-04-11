a1d42b2ae85317e8b236e45f1680562b26966821
Carlos Vázquez <36528993+cvazquezsa@users.noreply.github.com>
2018-04-10 19:37:18 -0500

    Cuentas contables Sucursal
    Se gregan campos en el catálogo de sucursales para capturar cuentas contables

---
+++ SQL/01-Tablas.sql
@@ -0,0 +1,6 @@
+EXEC spALTER_TABLE 'Sucursal','CuentaIVA','varchar(20) NULL'
+EXEC spALTER_TABLE 'Sucursal','CuentaVenta','varchar(20) NULL'
+EXEC spALTER_TABLE 'Sucursal','CuentaAlmacen','varchar(20) NULL'
+EXEC spALTER_TABLE 'Sucursal','CuentaCosto','varchar(20) NULL'
+EXEC spALTER_TABLE 'Sucursal','CuentaDevolucion','varchar(20) NULL'
+GO
