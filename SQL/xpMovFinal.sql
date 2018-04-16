214b1fb22e9116394b4c42c0451cc90f907a219a
Carlos Vázquez <36528993+cvazquezsa@users.noreply.github.com>
2018-04-16 18:33:23 -0500

    xpMovFinal integrado
    XpMovFinal Integrado

--- SQL/xpMovFinal.sql
+++ SQL/xpMovFinal.sql
@@ -8,53 +8,75 @@ SET ANSI_WARNINGS OFF
 
 IF NOT object_id('dbo.xpMovFinal', 'P') IS NULL DROP PROCEDURE dbo.xpMovFinal
 GO
-CREATE PROCEDURE xpMovFinal  
-@Empresa       char(5),  
-@Sucursal      int,  
-@Modulo        char(5),  
-@ID            int,  
-@Estatus       char(15),  
-@EstatusNuevo  char(15),  
-@Usuario       char(10),  
-@FechaEmision  datetime,  
-@FechaRegistro datetime,  
-@Mov           char(20),  
-@MovID         varchar(20),  
-@MovTipo       char(20),  
-@IDGenerar     int,  
-@Ok            int  OUTPUT,  
-@OkRef         varchar(255) OUTPUT  
+CREATE PROCEDURE xpMovFinal
+	@Empresa				char(5),
+	@Sucursal				int,
+	@Modulo					char(5),
+	@ID							int, 			
+	@Estatus				char(15),
+	@EstatusNuevo		char(15),
+	@Usuario				char(10),
+	@FechaEmision		datetime, 	
+	@FechaRegistro	datetime,
+	@Mov						char(20),
+	@MovID					varchar(20),
+	@MovTipo				char(20),
+	@IDGenerar			int,
+	@Ok							int		OUTPUT,
+	@OkRef					varchar(255)	OUTPUT
 AS BEGIN
-   DECLARE @SubClave  varchar(20)
 
-IF @Modulo='COMS'  
-BEGIN
-  IF @MovTipo='COMS.PR'
+	DECLARE @SubClave  varchar(20)
+
+  IF @EstatusNuevo IN ('PENDIENTE', 'CONCLUIDO', 'PROCESAR') AND (@Ok IS NULL OR @Ok BETWEEN 80030 AND 81000)
+    EXEC spReconstruirMovImpuesto @Modulo,@ID
+
+
+
+  IF @Modulo IN ('DIN') AND (@Ok IS NULL OR @Ok BETWEEN 80030 AND 81000)
   BEGIN
-    SELECT @SubClave=SubClave FROM Movtipo WHERE Mov=@Mov and Modulo=@Modulo
-    IF @SubClave='COMS.PRAUM'
-    BEGIN
-      EXEC spWFGAumentaPresupuesto @ID
-    END
-    IF @SubClave='COMS.PRDIS'
-    BEGIN
-      EXEC spWFGDisminuyePresupuesto @ID
-    END
+    EXEC xpActualizarContSATComprobante @Modulo, @ID
   END
-  IF @MovTipo='COMS.O' AND @EstatusNuevo='PENDIENTE'
+  IF EXISTS(SELECT ContX FROM EMPRESAGRAL WHERE Empresa = @Empresa AND ContX = 1) AND @Modulo = 'CONT' AND (@Ok IS NULL OR @Ok BETWEEN 80030 AND 81000)
   BEGIN
-    EXEC spWFGUpdateArtSubLinea @ID
-
+    EXEC spContSATConexionContable @Empresa,@Modulo,@ID
+	
   END
+  IF @Modulo IN('CXC','VTAS') AND (@Ok IS NULL OR @Ok BETWEEN 80030 AND 81000)
+    EXEC xpContSAT @Empresa,@Modulo,@ID
+  IF @Modulo IN('GAS','CXP','DIN')
+    EXEC spAsociacionRetroactiva @Modulo, @ID, @Empresa
+  EXEC spContSATMovFlujo @Empresa, @Sucursal, @Modulo, @ID, @Mov, @MovID, @Estatus
 
+  IF @Modulo IN ('VTAS','DIN')
+    EXEC xpAsociarNotasCorte @ID, @Mov, @Modulo, @Empresa, @Sucursal, @Estatus
+  IF @Modulo = 'DIN' AND (SELECT dbo.fnMovTipo(@Modulo, @Mov)) IN ('DIN.D','DIN.DE')
 
+	IF @Modulo='COMS'  
+	BEGIN
+
+		IF @MovTipo='COMS.PR'
+		BEGIN
+			SELECT @SubClave=SubClave FROM Movtipo WHERE Mov=@Mov and Modulo=@Modulo
+			IF @SubClave='COMS.PRAUM'
+			BEGIN
+				EXEC spWFGAumentaPresupuesto @ID
+			END
+			IF @SubClave='COMS.PRDIS'
+			BEGIN
+				EXEC spWFGDisminuyePresupuesto @ID
+			END
+		END
+		IF @MovTipo='COMS.O' AND @EstatusNuevo='PENDIENTE'
+		BEGIN
+			EXEC spWFGUpdateArtSubLinea @ID
+		END
+	END
+
+	EXEC xpAsociarCFDICobro @ID, @Empresa
+RETURN
 END
-
- 
-RETURN  
-END  
 GO
-
 
 
 
